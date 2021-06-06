8219ed1ae528542106857bf5dc6695b58dd1cac9
Kerem Gurel <keremgurell99@gmail.com>
2021-06-05 20:08:13 -0700

    depth-first searches

--- bst.js
+++ bst.js
@@ -62,12 +62,55 @@ class BinarySearchTree {
         queue.push(node);
         while(queue.length) {
             node = queue.shift();
-            data.push(node);
+            data.push(node.val);
             if(node.left) queue.push(node.left);
             if(node.right) queue.push(node.right);
         }
         return data;
     }
+    DFSPreOrder() {
+        var data = [];
+        function traverse(node) {
+            data.push(node.val);
+            if(node.left) {
+                traverse(node.left);
+            }
+            if(node.right) {
+                traverse(node.right);
+            }
+        }
+        traverse(this.root);
+        return data;
+    }
+    DFSPostOrder() {
+        var data = [];
+        function traverse(node) {
+            if(node.left) {
+                traverse(node.left);
+            }
+            if(node.right) {
+                traverse(node.right);
+            }
+            data.push(node.val);
+        }
+        traverse(this.root);
+        return data;
+    }
+    DFSInOrder() {
+        var data = [];
+        function traverse(node) {
+            if(node.left) {
+                traverse(node.left);
+            }
+            data.push(node.val);
+            if(node.right) {
+                traverse(node.right);
+            }
+        }
+        traverse(this.root);
+        return data;
+    }
+
 }
 
 var tree = new BinarySearchTree();
