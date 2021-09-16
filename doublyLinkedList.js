ec4eb653da62eecf19bc074c8ccf8e800cc4c7f1
Kerem Gurel <keremgurell99@gmail.com>
2021-09-15 22:30:01 -0700

    added final methods

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -1,5 +1,5 @@
 class Node {
-    construcor(val) {
+    constructor(val) {
         this.val = val;
         this.next = null;
         this.prev = null;
@@ -43,12 +43,103 @@ class DoublyLinkedList {
     }
 
     shift() {
+        let oldHead = this.head;
         if (!this.head) return undefined;
         if (this.length === 1) {
             this.head = null;
             this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
         }
-        let oldHead = this.head;
-        this.head = oldHead.next;
+
+        this.length--;
+        return oldHead;
+    }
+
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        if (index <= this.length / 2) {
+            let curr = this.head;
+            for (let i = 0; i < index; i++) {
+                curr = curr.next;
+            }
+            return curr;
+        } else {
+            let curr = this.tail;
+            for (let i = this.length - 1; i > index; i--) {
+                curr = curr.prev
+            }
+            return curr;
+        }
+    }
+
+    set(index, val) {
+        var foundNode = this.get(index);
+        if(foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+      return false;
+    }
+
+    insert(index, val){
+        if(index < 0 || index > this.length) return false;
+        if(index === 0) return this.unshift(val);
+        if(index === this.length) return this.push(val);
+    
+        let newNode = new Node(val);
+        let beforeNode = this.get(index-1);
+        let afterNode = beforeNode.next;
+        
+        beforeNode.next = newNode;
+        newNode.prev = beforeNode;
+        newNode.next = afterNode;
+        afterNode.prev = newNode;
+        this.length++;
+        return true;
+    }
+
+    remove(index) {
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+        let previousNode = this.get(index - 1);
+        let removed = previousNode.next;
+        previousNode.next = removed.next;
+        removed.prev = previousNode;
+        this.length--;
+        return removed;
     }
 }
+
+let kerem = new DoublyLinkedList();
+kerem.push('Harry')
+kerem.push('Potter')
+kerem.push('Kerem')
+kerem.push('Gurel')
+kerem.push('Can')
+kerem.push('Asim')
+kerem.push('Ali')
+kerem.push('Zeynep')
+kerem.push('Ege')
+kerem.get(7)
+kerem.unshift('New')
+kerem.insert(3, 'Three')
+console.log(kerem);
